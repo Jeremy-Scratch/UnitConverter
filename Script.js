e7347de1fccefd3b916e02e8934111800d422c40
Scratch <Jeremyscratch6@gmail.com>
2025-11-05 21:35:33 -0400

    Desing-upgrades_animationsImplements

---
+++ Script.js
@@ -0,0 +1,16 @@
+
+let unitType = document.getElementById('myDropdown');
+unitType.addEventListener('change',function () {
+    if(unitType.value==='Longitude'){
+        let options = document.getElementById('myDropdown1');
+        // options.value ===
+    }
+});
+
+
+const input = document.getElementById('input');
+input.addEventListener('input',function () {
+    let ff = input.value;
+console.log(ff);
+    
+});
